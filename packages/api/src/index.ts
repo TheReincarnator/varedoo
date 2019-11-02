24e11b9a6eff45bf880b5cf0635bd011716ff899
Thomas Jacob <git@thomasjacob.de>
2019-11-02 15:21:41 +0100

    working

--- packages/api/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as magic } from './test';
--- packages/server/src/domain/user.ts
+++ packages/server/src/domain/user.ts
@@ -1,5 +1,3 @@
-// import { magic } from 'varedoo-api';
-
-// import x from 'string_decoder';
+import magic from 'varedoo-api/lib/test';
 
 export class Wurst {}
