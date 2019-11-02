6a75e8f83db5dc1986f8b2e0a4663ecd2f1f00e7
Thomas Jacob <git@thomasjacob.de>
2019-11-02 18:47:54 +0100

    swagger-ui

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,26 @@
 import * as express from 'express';
+import { initialize } from 'express-openapi';
+import * as swaggerUi from 'swagger-ui-express';
+import * as apiDoc from 'varedoo-api/lib/api.json';
+
 import { getUsers } from './service/userService';
 
 const app = express();
 const port = 3000;
 
-app.get('/users', async (request, response) => {
-	response.json(await getUsers());
+initialize({
+	app,
+	apiDoc: '../api/src/api.json',
+	dependencies: {
+		log: console.log,
+	},
+	operations: {
+		getUsers: async (request, response) => {
+			response.json(await getUsers());
+		},
+	},
 });
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(apiDoc));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
